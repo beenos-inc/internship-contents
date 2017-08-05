c6dbbfcf88599d7d48853ce6ffed8aa152c48346
Hasegawa Yohei <yohey@ip-10-3-4-55.ap-northeast-1.compute.internal>
2017-08-05 16:27:31 +0900

    AirplanePlan Class BusPlan Class を追加
    TrainPlan Classを更新

--- YoheiHasegawa/PHASE3/step1/sample.php
+++ YoheiHasegawa/PHASE3/step1/sample.php
@@ -1,17 +1,32 @@
 <?php
 
 // PHASE.3 step.1 5つのクラスをrequireする
-
+require_once("AirplanePlan.php");
+require_once("BusPlan.php");
+require_once("HotelPlan.php");
+require_once("TrainPlan.php");
+require_once("TravelPrice.php");
 /**
  * 交通費を求める
  **/
-
+$airplane_plan = new AirplanePlan(30000);
+$airplane_price = $airplane_plan->useEarlyStageDiscount();
+$airplane_price = $airplane_plan->getRoundTripPrice();
 
 /**
  * ホテル代を求める
  **/
-
+$hotel_plan = new HotelPlan(8000);
+$hotel_plan->selectHotelRank("gold");
+$hotel_plan->eatBreakfast();
+$hotel_plan->eatDinner();
+$hotel_price = $hotel_plan->getTotalPrice();
 
 /**
  * 旅費合計を求める
  **/
+ $travel_price = new TravelPrice($airplane_price, $hotel_price);
+ $total_price = $travel_price->getTotalPrice();
+ echo "交通費 : " . $airplane_price . "円\n";
+ echo "宿泊費 : " . $hotel_price . "円\n";
+ echo "旅費合計 :" . $total_price . "円";
