72977ca22f8f3abb98caf55acc1b1314c60542b9
Hasegawa Yohei <yohey@ip-10-3-4-55.ap-northeast-1.compute.internal>
2017-08-05 17:09:45 +0900

    smaple.php にメインの処理を追加

---
+++ YoheiHasegawa/PHASE3/step2/sample.php
@@ -0,0 +1,22 @@
+<?php
+require_once('AirplanePlan.php');
+require_once('BusPlan.php');
+require_once('HotelPlan.php');
+require_once('TrainPlan.php');
+require_once('TravelPrice.php');
+
+$train_plan = new TrainPlan(12500);
+$train_plan->setStudentDiscountPercent(40);
+$train_plan->useStudentDiscount();
+$train_price = $train_plan->getRoundTripPrice();
+
+$hotel_plan = new HotelPlan(5000);
+$hotel_plan->useSmokerRoom();
+$hotel_plan->selectHotelRank('normal');
+$hotel_price = $hotel_plan->getTotalPrice();
+
+$travel_plan = new TrainPlan($train_price, $hotel_price);
+$total_price = $train_plan->getTotalPrice();
+echo "交通費 : " . $train_price . "円\n";
+echo "宿泊費 : " . $hotel_price . "円\n";
+echo "旅費合計 : " . $total_price . "円\n";
