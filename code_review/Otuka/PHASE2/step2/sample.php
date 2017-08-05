91ce74a18559d98a218dea74e58129eeb08770f4
unknown <s14039@tokyo.kosen-ac.jp>
2017-08-05 17:35:21 +0900

    ホテル代クラスをファイル分割

--- code_review/Otuka/PHASE2/step2/sample.php
+++ code_review/Otuka/PHASE2/step2/sample.php
@@ -1,5 +1,6 @@
 <?php
 require('TrainPlan.php');
+require('HotelPlan.php');
 // PHASE.2 step.2 3つのクラスをrequireする
 
 /**
@@ -17,6 +18,15 @@ echo "\n";
  * ホテル代を求める
  **/
 
+$hotel_plan = new HotelPlan(5000);
+$hotel_plan->useSmokingRoom();
+$hotel_plan->selectHotelRank('silver');
+$hotel_plan->eatBreakfast();
+$hotel_plan->eatDinner();
+$hotel_price = $hotel_plan->getTotalPrice();
+
+echo "ホテル代： " . $hotel_price . "円";
+echo "\n";
 
 /**
  * 旅費合計を求める
