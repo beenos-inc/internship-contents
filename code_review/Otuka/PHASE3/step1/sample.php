7185dc41c1c76a0a459f3a078b9e7b940fcab145
unknown <s14039@tokyo.kosen-ac.jp>
2017-08-05 23:10:16 +0900

    sample.phpにクラスを読み込んで，旅費を計算するプログラムの追加
    旅費を構成する要素をそれぞれ別のファイルにクラスとして定義することで，メインの処理を簡潔に記述できるため

--- code_review/Otuka/PHASE3/step1/sample.php
+++ code_review/Otuka/PHASE3/step1/sample.php
@@ -1,17 +1,36 @@
 <?php
 
 // PHASE.3 step.1 5つのクラスをrequireする
+require_once("BusPlan.php");
+require_once("AirplanePlan.php");
+require_once("TrainPlan.php");
+require_once("HotelPlan.php");
+require_once("TravelPrice.php");
 
 /**
  * 交通費を求める
  **/
+$airplane_plan = new AirplanePlan(30000);
+$airplane_plan->useEarlyDiscount();
+$airplane_price = $airplane_plan->getRoundTripPrice();
 
+echo "交通費 : " . $airplane_price . "円\n";
 
 /**
  * ホテル代を求める
  **/
+$hotel_plan = new HotelPlan(8000);
+$hotel_plan->selectHotelRank('gold');
+$hotel_plan->eatBreakfast();
+$hotel_plan->eatDinner();
+$hotel_price = $hotel_plan->getTotalPrice();
 
+echo "ホテル代 : " . $hotel_price . "円\n";
 
 /**
  * 旅費合計を求める
  **/
+$travel_plan = new TravelPrice($airplane_price, $hotel_price);
+$total_price = $travel_plan->getTotalPrice();
+
+echo "旅費合計 : " . $total_price . "円\n";
