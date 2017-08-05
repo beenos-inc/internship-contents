7aa61e587d42fb2b24ff2113cd6e70c4734caad8
Koki Sato <s.4115.koki@gmail.com>
2017-08-05 16:57:44 +0900

    copy files from PHASE3/step1
    in short, do this command.
    cp code_review/work/koki-sato/PHASE3/step1/* code_review/work/koki-sato/PHASE3/step2/

---
+++ code_review/work/koki-sato/PHASE3/step2/TrainPlan.php
@@ -0,0 +1,31 @@
+<?php
+
+class TrainPlan
+{
+    const STUDENT_DISCOUNT_COEFFICIENT = 0.2;
+    // PHASE.3 step.1 グリーン車代の定数を定義する
+    const GREEN_CAR_COST = 5000;
+    private $price = 0;
+
+
+    public function __construct($price)
+    {
+        $this->price = $price;
+    }
+
+    public function useStudentDiscount()
+    {
+        $this->price = floor($this->price * (1 - self::STUDENT_DISCOUNT_COEFFICIENT));
+    }
+
+    // PHASE.3 step.1 グリーン車を利用するメソッドを追加する
+    public function useGreenCar()
+    {
+        $this->price += self::GREEN_CAR_COST;
+    }
+
+    public function getRoundTripPrice()
+    {
+        return $this->price * 2;
+    }
+}
