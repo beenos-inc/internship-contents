04d5e287075112b09f6b1f61e260b70d42966e2c
qzpm <jliberecop@gmail.com>
2017-08-05 16:39:32 +0900

    ホテルの料金を計算するクラスを追加

--- code_review/work/kamimura/PHASE2/sample.php
+++ code_review/work/kamimura/PHASE2/sample.php
@@ -1,4 +1,5 @@
 <?php
+
 class TrainPlan
 {
     const STUDENT_DISCOUNT_COEFFICIENT = 0.2;
@@ -23,7 +24,65 @@ class TrainPlan
 
 }
 
+class HotelPlan
+{
+    // 喫煙ルームオプション代
+    const SMOKER_COST = 1000;
+    // ホテルの部屋別ランク料金
+    const HOTEL_ROOM_RANK_ADD_FEES = [
+        'normal' => 0,
+        'bronze' => 3000,
+        'silver' => 5000,
+        'gold' => 8000
+    ];
+    // 食事料金
+    const MEAL_ADD_FEES = [
+        'breakfast' => 500,
+        'dinner' => 800
+    ];
+
+    private $price = 0;
+
+
+    public function __construct($price)
+    {
+        $this->price = $price;
+    }
+
+    // PHASE.2 step.1 喫煙可能部屋を使うメソッドを追加する
+    public function useSmookingRoom()
+    {
+        $this->price += self::SMOKER_COST;
+    }
+    // PHASE.2 step.1 ホテルのランクを選択するメソッドを追加する
+    public function selectRank($rank)
+    {
+        $this->price += self::HOTEL_ROOM_RANK_ADD_FEES[$rank];
+    }
+    // PHASE.2 step.1 朝食を食べるメソッドを追加する
+    public function eatBreakfast()
+    {
+        $this->price += self::MEAL_ADD_FEES['breakfast'];
+    }
+    // PHASE.2 step.1 夕食を食べるメソッドを追加する
+    public function eatDinner()
+    {
+        $this->price += self::MEAL_ADD_FEES['dinner'];
+    } 
+    public function getTotalPrice()
+    {
+        return $this->price;
+    }
+}
+
 $train_price = new TrainPlan(12500);
 $train_price -> useStudentDiscount();
 
+$hotel_price = new HotelPlan(5000);
+$hotel_price -> useSmookingRoom();
+$hotel_price -> selectRank('silver');
+$hotel_price -> eatBreakfast();
+$hotel_price -> eatDinner();
+
 echo "交通費: " . $train_price -> getRoundTripPrice() . "円\n";
+echo "ホテル代: " . $hotel_price -> getTotalPrice() . "円\n";
