a05ba2a32f8f5785c8e4b8433a7187e8b984353b
unknown <s14039@tokyo.kosen-ac.jp>
2017-08-05 17:11:39 +0900

    まだ交通費クラス分岐の作成途中
    git commit --amend で対応できるため

--- code_review/Otuka/PHASE2/step2/sample.php
+++ code_review/Otuka/PHASE2/step2/sample.php
@@ -1,11 +1,17 @@
 <?php
-
+require('TrainPlan.php')
 // PHASE.2 step.2 3つのクラスをrequireする
 
 /**
  * 交通費を求める
  **/
 
+$train_plan = new TrainPlan(12500);
+$train_plan->useStudentDiscount();
+$round_trip_price = $train_plan->getRoundTripPrice();
+
+echo "交通費： " . $round_trip_price . "円";
+echo "\n";
 
 /**
  * ホテル代を求める
