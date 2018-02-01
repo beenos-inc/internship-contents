5e7568c0b296c7a48dc38cf0d951f9eca0dd809b
森角　拓紀 <morikaku_h@ip-10-3-8-102.ap-northeast-1.compute.internal>
2018-02-01 21:57:44 +0900

    メソッド名の変更、不要な変数への代入を削除、puts処理を下部にまとめ

--- code_review/morikaku/phase1-2.rb
+++ code_review/morikaku/phase1-2.rb
@@ -1,25 +1,26 @@
-def calculate_transportation_expences(basic_single_fare)
+def calculate_round_trip_fare(basic_single_fare)
   student_discount  = basic_single_fare * 0.2
   total_single_fare = basic_single_fare - student_discount
 
-  round_trip_fare = (total_single_fare * 2).round
+  (total_single_fare * 2).round
 end
 
 # ()内には片道の基本料金を入力
-transportation_expences = calculate_transportation_expences(12500)
-puts "学割適用後の往復合計金額は#{transportation_expences}円です。"  #=> 学割適用後の往復合計金額は20000円です。
-
+round_trip_fare = calculate_round_trip_fare(12500)
 
 def calculate_hotel_expences(basic_hotel_expences)
   optional_breakfast_fee         = 500
   optional_dinner_fee            = 800
   total_breakfast_and_dinner_fee = optional_breakfast_fee + optional_dinner_fee
 
-  total_hotel_expences = basic_hotel_expences + total_breakfast_and_dinner_fee
+  basic_hotel_expences + total_breakfast_and_dinner_fee
 end
 
 # ()内にはホテルの基本料金を入力
 hotel_expences = calculate_hotel_expences(5000)
+
+puts "学割適用後の往復運賃は#{round_trip_fare}円です。"  #=> 学割適用後の往復運賃は20000円です。
+
 puts "ホテルの宿泊料金は朝食・夕食代金合わせて#{hotel_expences}円です。"  #=> ホテルの宿泊料金は朝食・夕食代金合わせて6300円です。
 
-puts "交通費とホテル代金の合計は#{transportation_expences + hotel_expences}円です。"  #=> 交通費とホテル代金の合計は26300円です。
+puts "往復運賃とホテル代金の合計は#{round_trip_fare + hotel_expences}円です。"  #=> 往復運賃とホテル代金の合計は26300円です。
