3f5998e652cf8cf06300b8c0c845081c28da8da7
森角　拓紀 <morikaku_h@ip-10-3-8-102.ap-northeast-1.compute.internal>
2018-02-02 21:58:14 +0900

    initializeメソッドへの変更、クラス定義のみのファイルとするためインスタンスの作成〜結果の出力を削除

--- code_review/morikaku/phase2-1.rb
+++ code_review/morikaku/phase2-1.rb
@@ -1,25 +1,15 @@
 # 交通費の計算をするクラスを作成
 class TrainPlan
   
-  attr_accessor :basic_single_fare
-
-  def student_discount_single_fare
-    (self.basic_single_fare * 0.8).round
+  def initialize(basic_single_fare)
+    @basic_single_fare = basic_single_fare
   end
 
-  def student_discount_round_trip_fare
-    student_discount_single_fare * 2
+  def student_discount_round_trip_fare_info
+    "学割適用後の往復運賃は#{(@basic_single_fare * 0.8 * 2).round}円です"
   end
 
 end
-
-round_trip_fare = TrainPlan.new
-
-# 片道の基本料金を入力
-round_trip_fare.basic_single_fare = 12500
-
-puts "学割適用後の往復運賃は#{round_trip_fare.student_discount_round_trip_fare}円です。"
-
 
 # 宿泊費を計算するクラスを作成
 class HotelPlan
