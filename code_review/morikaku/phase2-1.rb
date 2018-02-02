b680e941d690b3500ed4bdff49559f3ecf77260e
森角　拓紀 <morikaku_h@ip-10-3-8-102.ap-northeast-1.compute.internal>
2018-02-03 00:34:39 +0900

    ホテルの宿泊料金を計算するHotelPlanクラスをinitializeメソッドを使用した記述に変更

--- code_review/morikaku/phase2-1.rb
+++ code_review/morikaku/phase2-1.rb
@@ -19,14 +19,16 @@ end
 
 # 宿泊費を計算するクラスを作成
 class HotelPlan
-
-  attr_accessor :choose_smoking_room
-  attr_accessor :choose_rank_of_room
-  attr_accessor :choose_breakfast
-  attr_accessor :choose_dinner
+  def initialize(basic_hotel_fee:, choose_smoking_room:, choose_rank_of_room:, choose_breakfast:, choose_dinner:)
+    @basic_hotel_fee     = basic_hotel_fee
+    @choose_smoking_room = choose_smoking_room
+    @choose_rank_of_room = choose_rank_of_room
+    @choose_breakfast    = choose_breakfast
+    @choose_dinner       = choose_dinner
+  end
 
   def smoking_room_fee
-    if self.choose_smoking_room == "yes"
+    if @choose_smoking_room == "yes"
       1000
     else
       0
@@ -34,11 +36,11 @@ class HotelPlan
   end
 
   def rank_of_room_fee
-    if self.choose_rank_of_room    == "gold"
+    if @choose_rank_of_room    == "gold"
       8000
-    elsif self.choose_rank_of_room == "silver"
+    elsif @choose_rank_of_room == "silver"
       5000
-    elsif self.choose_rank_of_room == "bronze"
+    elsif @choose_rank_of_room == "bronze"
       3000
     else
       0
@@ -46,7 +48,7 @@ class HotelPlan
   end
 
   def breakfast_fee
-    if self.choose_breakfast == "yes"
+    if @choose_breakfast == "yes"
       500
     else
       0
@@ -54,33 +56,14 @@ class HotelPlan
   end
 
   def dinner_fee
-    if self.choose_dinner == "yes"
+    if @choose_dinner == "yes"
       800
     else
       0
     end
   end
 
+  def total_hotel_fee_info
+    "ホテルの宿泊料金は#{@basic_hotel_fee + smoking_room_fee + rank_of_room_fee + breakfast_fee + dinner_fee}円です。"
+  end
 end
-
-smoking_room = HotelPlan.new
-rank_of_room = HotelPlan.new
-breakfast    = HotelPlan.new
-dinner       = HotelPlan.new
-
-# smoking_roomの使用有無を選択。""内にyes / noを入力
-smoking_room.choose_smoking_room = "no"
-
-# 部屋のランクを選択。""内にgold / silver / bronze / normalのいずれかを入力
-rank_of_room.choose_rank_of_room = "normal"
-
-# 朝食の必要有無を選択。""内にyes / noを入力
-breakfast.choose_breakfast = "no"
-
-# 夕食の必要有無を選択。""内にyes / noを入力
-dinner.choose_dinner = "no"
-
-basic_hotel_fee = 5000
-total_hotel_fee = basic_hotel_fee + smoking_room.smoking_room_fee + rank_of_room.rank_of_room_fee + breakfast.breakfast_fee + dinner.dinner_fee
-
-puts "ホテルの宿泊料金は#{total_hotel_fee}円です。"
