189e8cdeab567cb0802ccb0aeadba4fdcf78de14
森角　拓紀 <morikaku_h@ip-10-3-8-102.ap-northeast-1.compute.internal>
2018-02-02 22:59:03 +0900

    運賃計算と出力結果を実行するメソッドを分けた

--- code_review/morikaku/phase2-1.rb
+++ code_review/morikaku/phase2-1.rb
@@ -5,8 +5,16 @@ class TrainPlan
     @basic_single_fare = basic_single_fare
   end
 
+  def student_discount_single_fare
+    (@basic_single_fare * 0.8).round
+  end
+
+  def student_discount_round_trip_fare
+    student_discount_single_fare * 2
+  end
+
   def student_discount_round_trip_fare_info
-    "学割適用後の往復運賃は#{(@basic_single_fare * 0.8 * 2).round}円です"
+    "学割適用後の往復運賃は#{student_discount_round_trip_fare}円です"
   end
 
 end
