6ae56093d5fe81936c329d355c4601c376648f46
森角　拓紀 <morikaku_h@ip-10-3-8-102.ap-northeast-1.compute.internal>
2018-02-07 17:00:58 +0900

    HotelPlanクラスを別ファイルへ分割

---
+++ code_review/morikaku/HotelPlan.rb
@@ -0,0 +1,37 @@
+# ホテル料金を計算するクラス
+class HotelPlan
+  def initialize(basic_hotel_fee:, choose_smoking_room:, choose_rank_of_room:, choose_breakfast:, choose_dinner:)
+    @basic_hotel_fee     = basic_hotel_fee
+    @choose_smoking_room = choose_smoking_room
+    @choose_rank_of_room = choose_rank_of_room
+    @choose_breakfast    = choose_breakfast
+    @choose_dinner       = choose_dinner
+  end
+
+  def smoking_room_fee
+    (@choose_smoking_room == "yes") ? 1_000 : 0
+  end
+
+  def rank_of_room_fee
+    return 8_000 if @choose_rank_of_room == "gold"
+    return 5_000 if @choose_rank_of_room == "silver"
+    return 3_000 if @choose_rank_of_room == "bronze"
+    0
+  end
+
+  def breakfast_fee
+    (@choose_breakfast == "yes") ? 500 : 0
+  end
+
+  def dinner_fee
+    (@choose_dinner == "yes") ? 800 : 0
+  end
+
+  def hotel_fee_include_options
+    @basic_hotel_fee + smoking_room_fee + rank_of_room_fee + breakfast_fee + dinner_fee
+  end
+
+  def hotel_fee_include_options_info
+    "ホテル料金は#{hotel_fee_include_options}円です。"
+  end
+end
