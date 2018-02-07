9b0f11be618546b0b7dafec03e6484858d2976bd
森角　拓紀 <morikaku_h@ip-10-3-8-102.ap-northeast-1.compute.internal>
2018-02-07 17:27:06 +0900

    往復料金を表示する処理がTravelPlanクラス内の処理と重複しているため削除

--- code_review/morikaku/TrainPlan.rb
+++ code_review/morikaku/TrainPlan.rb
@@ -11,8 +11,4 @@ class TrainPlan
   def student_discount_round_trip_fare
     student_discount_single_fare * 2
   end
-
-  def student_discount_round_trip_fare_info
-    "学割適用後の往復運賃は#{student_discount_round_trip_fare}円です"
-  end
 end
