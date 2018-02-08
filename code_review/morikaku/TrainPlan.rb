61739357f97fd73be00a83c4943b7fcbe78ce04c
森角　拓紀 <morikaku_h@ip-10-3-8-102.ap-northeast-1.compute.internal>
2018-02-08 17:32:22 +0900

    グリーン車を使用した場合の追加料金の計算を追加

--- code_review/morikaku/TrainPlan.rb
+++ code_review/morikaku/TrainPlan.rb
@@ -1,14 +1,20 @@
-#往復運賃の計算をするクラス
+#往復運賃の計算をするクラス（グリーン車を選択するメソッドを追加）
 class TrainPlan
-  def initialize(basic_single_fare:)
+  def initialize(basic_single_fare:, choose_green_car:)
     @basic_single_fare = basic_single_fare
+    @choose_green_car = choose_green_car
   end
 
   def student_discount_single_fare
     (@basic_single_fare * 0.8).round
   end
 
-  def student_discount_round_trip_fare
-    student_discount_single_fare * 2
+  def student_discount_single_fare_include_green_car_fare
+    (@choose_green_car == "yes") ? student_discount_single_fare + 5000 : student_discount_single_fare
+  end
+
+  def student_discount_round_trip_fare_include_green_car_fare
+    student_discount_single_fare_include_green_car_fare * 2
   end
 end
+
