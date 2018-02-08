c55cb30e31a8a95ce9da76db67da0d16c8635005
森角　拓紀 <morikaku_h@ip-10-3-8-102.ap-northeast-1.compute.internal>
2018-02-08 17:41:04 +0900

    AirplanePlanクラスとBusPlanクラスの引数の追加、両者の計算結果出力実行コードの追加

--- code_review/morikaku/main.rb
+++ code_review/morikaku/main.rb
@@ -1,14 +1,21 @@
 require "./TravelPlan"
 
 travel_plan = TravelPlan.new(
-  basic_single_fare:   12_500,
-  basic_hotel_fee:     5_000,
-  choose_smoking_room: "yes",
-  choose_rank_of_room: "gold",
-  choose_breakfast:    "yes",
-  choose_dinner:       "yes"
+  basic_single_fare:             12_500,
+  choose_green_car:              "yes",
+  basic_hotel_fee:               5_000,
+  choose_smoking_room:           "yes",
+  choose_rank_of_room:           "gold",
+  choose_breakfast:              "yes",
+  choose_dinner:                 "yes",
+  basic_airplane_fare:           20_000,
+  choose_early_booking_discount: "yes",
+  basic_bus_fare:                1_000,
+  choose_coupon_discount:        "yes"
 )
 
 puts travel_plan.total_train_fare_info
 puts travel_plan.total_hotel_fee_info
+puts travel_plan.total_airplane_fare_info
+puts travel_plan.total_bus_fare_info
 puts travel_plan.total_travel_fee_info
