aa912df32772d8000a793bd751df65551586367c
森角　拓紀 <morikaku_h@ip-10-3-8-102.ap-northeast-1.compute.internal>
2018-02-09 18:03:54 +0900

    BusPlanクラス 学割を任意の値で計算するメソッドの追加、他のクラスと統一性を持たせるためメソッド名・仮引数名を一部変更。

--- code_review/morikaku/BusPlan.rb
+++ code_review/morikaku/BusPlan.rb
@@ -1,11 +1,24 @@
-# バス料金を計算するBusPlanクラス
+# バスの往復料金の計算（学割、クーポン割含む）をするBusPlanクラス
 class BusPlan
-  def initialize(basic_bus_fare:, choose_coupon_discount:)
-    @basic_bus_fare = basic_bus_fare
-    @choose_coupon_discount = choose_coupon_discount
+  def initialize(bus_single_fare:, bus_student_discount_rate:, choose_coupon_discount:)
+    @bus_single_fare           = bus_single_fare
+    @bus_student_discount_rate = bus_student_discount_rate
+    @choose_coupon_discount    = choose_coupon_discount
   end
 
-  def bus_fare_include_coupon_discount
-    (@choose_coupon_discount == "yes") ? (@basic_bus_fare * 0.9).round : @basic_bus_fare
+  def student_discount_bus_single_fare
+    if @bus_student_discount_rate > 0 && @bus_student_discount_rate <= 100
+      (@bus_single_fare * (1 - @bus_student_discount_rate / 100.0)).round
+    else
+      puts "学割の値は0より大きく100未満の値を入れてください。"
+    end
+  end
+
+  def coupon_discount_bus_single_fare
+    (@choose_coupon_discount == "yes") ? (student_discount_bus_single_fare * 0.9).round : student_discount_bus_single_fare
+  end
+
+  def bus_round_trip_fare_include_discount
+    coupon_discount_bus_single_fare * 2
   end
 end
