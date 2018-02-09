ff37c4790edb95a84d90a2d9a063bbf1e5547725
森角　拓紀 <morikaku_h@ip-10-3-8-102.ap-northeast-1.compute.internal>
2018-02-09 17:29:59 +0900

    学割を任意の値で計算するメソッドの追加、他のクラスと統一性を持たせるため、メソッド名・引数名を一部変更。グリーン車の追加料金計算メソッドを個別に作成しなおした。

--- code_review/morikaku/TrainPlan.rb
+++ code_review/morikaku/TrainPlan.rb
@@ -1,20 +1,29 @@
-#往復運賃の計算をするクラス（グリーン車を選択するメソッドを追加）
+#電車の往復料金の計算（学割、グリーン車追加料金含む）をするTrainPlanクラス
 class TrainPlan
-  def initialize(basic_single_fare:, choose_green_car:)
-    @basic_single_fare = basic_single_fare
-    @choose_green_car = choose_green_car
+  def initialize(train_single_fare:, train_student_discount_rate:, choose_green_car:)
+    @train_single_fare           = train_single_fare
+    @train_student_discount_rate = train_student_discount_rate
+    @choose_green_car            = choose_green_car
   end
 
-  def student_discount_single_fare
-    (@basic_single_fare * 0.8).round
+  def student_discount_train_single_fare
+    if @train_student_discount_rate > 0 && @train_student_discount_rate <= 100
+      (@train_single_fare * (1 - @train_student_discount_rate / 100.0)).round
+    else
+      puts "学割の値は0より大きく100未満の値を入れてください。"
+    end
   end
 
-  def student_discount_single_fare_include_green_car_fare
-    (@choose_green_car == "yes") ? student_discount_single_fare + 5000 : student_discount_single_fare
+  def green_car_fare
+    (@choose_green_car == "yes") ? 5_000 : 0
   end
 
-  def student_discount_round_trip_fare_include_green_car_fare
-    student_discount_single_fare_include_green_car_fare * 2
+  def student_discount_train_single_fare_include_green_car_fare
+    student_discount_train_single_fare + green_car_fare
+  end
+
+  def train_round_trip_fare_include_green_car_fare
+    student_discount_train_single_fare_include_green_car_fare * 2
   end
 end
 
