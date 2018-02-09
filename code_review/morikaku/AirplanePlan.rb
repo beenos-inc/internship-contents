a807f808185970928cfe9bef68d94d482a76f917
森角　拓紀 <morikaku_h@ip-10-3-8-102.ap-northeast-1.compute.internal>
2018-02-09 17:58:53 +0900

    学割を任意の値で計算するメソッドの追加、他のクラスと統一性を持たせるためメソッド名・仮引数名を一部変更。

--- code_review/morikaku/AirplanePlan.rb
+++ code_review/morikaku/AirplanePlan.rb
@@ -1,11 +1,24 @@
-# 飛行機料金を計算するAirplanePlanクラス
+# 飛行機の往復料金の計算（学割、早割含む）をするAirplanePlanクラス
 class AirplanePlan
-  def initialize(basic_airplane_fare:, choose_early_booking_discount:)
-    @basic_airplane_fare = basic_airplane_fare
-    @choose_early_booking_discount = choose_early_booking_discount
+  def initialize(airplane_single_fare:, airplane_student_discount_rate:, choose_early_booking_discount:)
+    @airplane_single_fare           = airplane_single_fare
+    @airplane_student_discount_rate = airplane_student_discount_rate
+    @choose_early_booking_discount  = choose_early_booking_discount
   end
 
-  def airplane_fare_include_early_booking_discount
-    (@choose_early_booking_discount == "yes") ? (@basic_airplane_fare * 0.7).round : @basic_airplane_fare
+  def student_discount_airplane_single_fare
+    if @airplane_student_discount_rate > 0 && @airplane_student_discount_rate <= 100
+      (@airplane_single_fare * (1 - @airplane_student_discount_rate / 100.0)).round
+    else
+      puts "学割の値は0より大きく100未満の値を入れてください。"
+    end
+  end
+
+  def early_booking_discount_airplane_single_fare
+    (@choose_early_booking_discount == "yes") ? (student_discount_airplane_single_fare * 0.7).round : student_discount_airplane_single_fare 
+  end
+
+  def airplane_round_trip_fare_include_discount
+    early_booking_discount_airplane_single_fare * 2
   end
 end
