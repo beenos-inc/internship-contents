bd743818cac04d6999e5ed421f42f4b982b46171
森角　拓紀 <morikaku_h@ip-10-3-8-102.ap-northeast-1.compute.internal>
2018-02-09 18:08:01 +0900

    各クラスで追加した仮引数の追加、読み込み先のクラスで変更したメソッド名・仮引数名を一致させる

--- code_review/morikaku/TravelPlan.rb
+++ code_review/morikaku/TravelPlan.rb
@@ -3,28 +3,32 @@ require "./HotelPlan"
 require "./AirplanePlan"
 require "./BusPlan"
 
-# 往復運賃とホテル料金を合計した旅費を計算するクラス
+# 電車、飛行機、バス料金とホテル料金を合計した旅費を計算するTravelPlanクラス
 class TravelPlan
-  def initialize(basic_single_fare:, choose_green_car:, basic_hotel_fee:, choose_smoking_room:, choose_rank_of_room:, choose_breakfast:, choose_dinner:, basic_airplane_fare:, choose_early_booking_discount:, basic_bus_fare:, choose_coupon_discount:)
-    @basic_single_fare             = basic_single_fare
-    @choose_green_car              = choose_green_car
-    @basic_hotel_fee               = basic_hotel_fee
-    @choose_smoking_room           = choose_smoking_room
-    @choose_rank_of_room           = choose_rank_of_room
-    @choose_breakfast              = choose_breakfast
-    @choose_dinner                 = choose_dinner
-    @basic_airplane_fare           = basic_airplane_fare
-    @choose_early_booking_discount = choose_early_booking_discount
-    @basic_bus_fare                = basic_bus_fare
-    @choose_coupon_discount        = choose_coupon_discount
+  def initialize(train_single_fare:, train_student_discount_rate:, choose_green_car:, basic_hotel_fee:, choose_smoking_room:, choose_rank_of_room:, choose_breakfast:, choose_dinner:, airplane_single_fare:, airplane_student_discount_rate:, choose_early_booking_discount:, bus_single_fare:, bus_student_discount_rate:, choose_coupon_discount:)
+    @train_single_fare              = train_single_fare
+    @train_student_discount_rate    = train_student_discount_rate
+    @choose_green_car               = choose_green_car
+    @basic_hotel_fee                = basic_hotel_fee
+    @choose_smoking_room            = choose_smoking_room
+    @choose_rank_of_room            = choose_rank_of_room
+    @choose_breakfast               = choose_breakfast
+    @choose_dinner                  = choose_dinner
+    @airplane_single_fare           = airplane_single_fare
+    @airplane_student_discount_rate = airplane_student_discount_rate
+    @choose_early_booking_discount  = choose_early_booking_discount
+    @bus_single_fare                = bus_single_fare
+    @bus_student_discount_rate      = bus_student_discount_rate
+    @choose_coupon_discount         = choose_coupon_discount
   end
 
   def total_train_fare
     train_plan = TrainPlan.new(
-      basic_single_fare: @basic_single_fare,
-      choose_green_car:  @choose_green_car
+      train_single_fare:           @train_single_fare,
+      train_student_discount_rate: @train_student_discount_rate,
+      choose_green_car:            @choose_green_car
     )
-    train_plan.student_discount_round_trip_fare_include_green_car_fare
+    train_plan.train_round_trip_fare_include_green_car_fare
   end
 
   def total_hotel_fee
@@ -40,18 +44,20 @@ class TravelPlan
 
   def total_airplane_fare
     airplane_plan = AirplanePlan.new(
-      basic_airplane_fare:           @basic_airplane_fare,
-      choose_early_booking_discount: @choose_early_booking_discount
+      airplane_single_fare:           @airplane_single_fare,
+      airplane_student_discount_rate: @airplane_student_discount_rate,
+      choose_early_booking_discount:  @choose_early_booking_discount
     )
-    airplane_plan.airplane_fare_include_early_booking_discount
+    airplane_plan.airplane_round_trip_fare_include_discount
   end
 
   def total_bus_fare
     bus_plan = BusPlan.new(
-      basic_bus_fare:         @basic_bus_fare,
-      choose_coupon_discount: @choose_coupon_discount
+      bus_single_fare:           @bus_single_fare,
+      bus_student_discount_rate: @bus_student_discount_rate,
+      choose_coupon_discount:    @choose_coupon_discount
     )
-    bus_plan.bus_fare_include_coupon_discount
+    bus_plan.bus_round_trip_fare_include_discount
   end
 
   def total_travel_fee
@@ -59,19 +65,19 @@ class TravelPlan
   end
 
   def total_train_fare_info
-    "往復運賃（学割適用、グリーン車料金含む）は#{total_train_fare}円です。"
+    "往復運賃  （学割、グリーン車料金含む）#{total_train_fare}円です。"
   end
 
   def total_hotel_fee_info
-    "ホテル料金（各種オプション料金含む）は#{total_hotel_fee}円です。"
+    "ホテル料金（各種オプション料金含む）  #{total_hotel_fee}円です。"
   end
 
   def total_airplane_fare_info
-    "飛行機料金（早割含む）は#{total_airplane_fare}円です。"
+    "飛行機料金（学割、早割含む）          #{total_airplane_fare}円です。"
   end
 
   def total_bus_fare_info
-    "バス料金（クーポン割引含む）は#{total_bus_fare}円です。"
+    "バス料金  （学割、クーポン割引含む）   #{total_bus_fare}円です。"
   end
 
   def total_travel_fee_info
