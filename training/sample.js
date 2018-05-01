cd17f23153fcd85d2257e5303eacf77ef6ab251f
kawasaki-t <kawasaki_t@tenso.com>
2018-05-01 10:03:27 +0900

    未入力項目のバリデーションを追加

--- training/sample.js
+++ training/sample.js
@@ -1,8 +1,24 @@
+function validation() {
+  var elements = document.querySelectorAll('input[type="text"]');
+  var bool = [...elements].some(element => {
+    if (element.value === '') {
+      return false;
+    }
+    return true;
+  });
+
+  if (!bool) alert('未入力項目があります');
+  return bool;
+}
+
 function clearInput() {
-  document.querySelector('#reset').addEventListener("click", function() {
-    elements = document.querySelectorAll('input[type="checkbox"]');
-    [...elements].map(element => {
-      element.checked = false;
-    });
+  var checkboxes = document.querySelectorAll('input[type="checkbox"]');
+  [...checkboxes].map(checkbox => {
+    checkbox.checked = false;
+  });
+
+  var texts = document.querySelectorAll('input[type="text"]');
+  [...texts].map(text => {
+    text.value = "";
   });
 }
