bfea56d90301e1c4f3c589c4b15eb095f4b54aad
kawasaki-t <kawasaki_t@tenso.com>
2018-05-10 10:58:25 +0900

    サニタイズにtrimを追加

--- training/functions.php
+++ training/functions.php
@@ -1,14 +1,14 @@
 <?php
-const NUMBER_OF_QUESTION = 5;
+  const NUMBER_OF_QUESTION = 5;
 
   function sanitize($target) {
     if (is_array($target)) {
       foreach ($target as $value) {
-        $array[] = htmlspecialchars($value, ENT_QUOTES);
+        $array[] = trim(htmlspecialchars($value, ENT_QUOTES));
       }
       return $array;
     } else {
-      return htmlspecialchars($target, ENT_QUOTES);
+      return trim(htmlspecialchars($target, ENT_QUOTES));
     }
   }
 ?>
