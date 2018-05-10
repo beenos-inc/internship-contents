6c09a29f50534a729b187dff8ff2d844264d7673
kawasaki-t <kawasaki_t@tenso.com>
2018-05-10 11:43:49 +0900

    問題文を作成する処理を簡略化

--- training/funcExercise.php
+++ training/funcExercise.php
@@ -4,46 +4,53 @@
   class funcExercise {
     private $max;
     private $output;
+    private $num1 = 0;
+    private $num2 = 0;
     function __construct($max, $output) {
       $this->max = $max;
       $this->output = $output;
     }
 
-    private function generateAdditionFormula($num1, $num2) {
-      $formula[$num1.' + '.$num2.' = '] = $num1 + $num2;
-      return $formula;
-    }
-    private function generateSubtractionFormula($num1, $num2) {
-      $formula[$num1.' - '.$num2.' = '] = $num1 - $num2;
-      return $formula;
-    }
-    private function generateUnsettledFormula($num1, $num2) {
-      return rand(0 ,1) ? $this->generateAdditionFormula($num1, $num2) : $this->generateSubtractionFormula($num1, $num2);;
-    }
-
     public function generateFormula() {
       $formula = [];
+      for ($i=0; $i < NUMBER_OF_QUESTION; $i++) { 
+        $formula += $this->decideFormulaBranch();
+      }
+      return $formula;
+    }
+
+    private function decideFormulaBranch() {
+      $this->rand();
       switch ($this->output) {
         case 1:
-          for ($i=0; $i < NUMBER_OF_QUESTION; $i++) { 
-            $formula = $formula + $this->generateAdditionFormula(rand(0, $this->max), rand(0, $this->max));
-          }
+            return $this->generateAdditionFormula();
           break;
         case 2:
-          for ($i=0; $i < NUMBER_OF_QUESTION; $i++) { 
-            $formula = $formula + $this->generateSubtractionFormula(rand(0, $this->max), rand(0, $this->max));
-          }
+            return $this->generateSubtractionFormula();
           break;
         case 3:
-          for ($i=0; $i < NUMBER_OF_QUESTION; $i++) { 
-            $formula = $formula + $this->generateUnsettledFormula(rand(0, $this->max), rand(0, $this->max));
-          }
+            return $this->generateUnsettledFormula();
           break;
         
         default:
+            return array();
           break;
       }
-      return $formula;
+    }
+
+    private function generateAdditionFormula() {
+      return array($this->num1.' + '.$this->num2.' = ' => $this->num1 + $this->num2);
+    }
+    private function generateSubtractionFormula() {
+      return array($this->num1.' - '.$this->num2.' = ' => $this->num1 - $this->num2);
+    }
+    private function generateUnsettledFormula() {
+      return rand(0 ,1) ? $this->generateAdditionFormula() : $this->generateSubtractionFormula();;
+    }
+
+    private function rand() {
+      $this->num1 = rand(0, $this->max);
+      $this->num2 = rand(0, $this->max);
     }
   }
 ?>
