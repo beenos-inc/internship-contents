9a865862f25c5d4a2844a4038084699885d1e52b
omija <oomija@tenso.com>
2018-07-04 17:27:48 +0900

    コメントを追加、コメント位置の修正

--- code_review/question/PHASE2/omija/step1/omija-phase2.rb
+++ code_review/question/PHASE2/omija/step1/omija-phase2.rb
@@ -1,3 +1,4 @@
+# 電車料金を算出するクラス
 class TrainPlan
     STUDENT_DISCOUNT_COEFFICIENT = 0.2
     
@@ -5,11 +6,13 @@ class TrainPlan
         @single_trip_price = single_trip_price
     end
 
+    # 学割料金を算出するメソッド
     def use_student_discount
-        @single_trip_price = (@single_trip_price * (1 - STUDENT_DISCOUNT_COEFFICIENT)).to_i # PHASE.2 step.1 学割料金を求めるメソッドを完成させる
+        @single_trip_price = (@single_trip_price * (1 - STUDENT_DISCOUNT_COEFFICIENT)).to_i
     end
 
-    def get_round_trip_price # PHASE.2 step.1 往復代を求めるメソッドを追加する
+    # 往復代を算出するメソッド
+    def get_round_trip_price 
         @single_trip_price
     end
 end
@@ -18,6 +21,7 @@ train_plan = TrainPlan.new(12500)
 train_plan.use_student_discount
 puts "交通費: #{train_plan.get_round_trip_price}円"    
 
+# ホテル料金を算出するクラス
 class HotelPlan
     SMOKER_COST = 1000
     HOTEL_ROOM_RANK_ADD_FEES = {
@@ -35,12 +39,12 @@ class HotelPlan
         @hotel_price = price
     end
 
-    # PHASE.2 step.1 喫煙可能部屋を使うメソッドを追加する
+    # 喫煙可能部屋を利用時のコストを取得するメソッド
     def is_smoker
         @hotel_price += SMOKER_COST
     end
 
-    # PHASE.2 step.1 ホテルのランクを選択するメソッドを追加する
+    # ホテルランクのコストを取得するメソッド
     def select_hotel_rank(hotel_room_rank)
         if HOTEL_ROOM_RANK_ADD_FEES.fetch(hotel_room_rank, nil).nil?
             p "正しいランクを選択してください"
@@ -50,16 +54,17 @@ class HotelPlan
         end
     end
 
-    # PHASE.2 step.1 朝食を食べるメソッドを追加する
+    # 朝食利用時のコストを取得するメソッド
     def has_breakfast
         @hotel_price += MEAL_ADD_FEES["breakfast"]
     end
  
-    # PHASE.2 step.1 夕食を食べるメソッドを追加する
+    # 夕食利用時のコストを取得するメソッド
     def has_dinner
         @hotel_price += MEAL_ADD_FEES["dinner"]
     end
 
+    # ホテル料金合計を算出するメソッド
     def get_hotel_price
         @hotel_price
     end
@@ -72,14 +77,15 @@ hotel_plan.has_breakfast
 hotel_plan.has_dinner
 puts "ホテル代: #{train_plan.get_round_trip_price}円"
 
+# 旅費合計を算出するクラス
 class TravelPrice
     def initialize(hotel_price, round_trip_price)
         @hotel_price = hotel_price
         @single_trip_price = round_trip_price
     end
 
+    # 旅費合計を算出するメソッド
     def get_total_price
-        # PHASE.2 step.1 配列の合計金額を求める処理を完成させる
         total_price = @hotel_price + @single_trip_price
         "旅費合計: #{total_price}円"
     end
