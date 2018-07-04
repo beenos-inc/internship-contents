e9bf6ee8e4f48f2373c6883b7d2abe13e9af4473
omija <oomija@tenso.com>
2018-07-04 17:44:43 +0900

    インデントの幅を調整

--- code_review/question/PHASE2/omija/step1/omija-phase2.rb
+++ code_review/question/PHASE2/omija/step1/omija-phase2.rb
@@ -1,20 +1,20 @@
 # 電車料金を算出するクラス
 class TrainPlan
-    STUDENT_DISCOUNT_COEFFICIENT = 0.2
-    
-    def initialize(single_trip_price)
-        @single_trip_price = single_trip_price
-    end
+  STUDENT_DISCOUNT_COEFFICIENT = 0.2
 
-    # 学割料金を算出するメソッド
-    def use_student_discount
-        @stuent_discount_cost = (@single_trip_price * STUDENT_DISCOUNT_COEFFICIENT).to_i
-    end
+  def initialize(single_trip_price)
+    @single_trip_price = single_trip_price
+  end
 
-    # 往復代を算出するメソッド
-    def round_trip_price 
-        @round_trip_price = (@single_trip_price - @stuent_discount_cost) * 2
-    end
+  # 学割料金を算出するメソッド
+  def use_student_discount
+    @stuent_discount_cost = (@single_trip_price * STUDENT_DISCOUNT_COEFFICIENT).to_i
+  end
+
+  # 往復代を算出するメソッド
+  def round_trip_price 
+    @round_trip_price = (@single_trip_price - @stuent_discount_cost) * 2
+  end
 end
 
 train_plan = TrainPlan.new(12500)
@@ -23,51 +23,51 @@ puts "交通費: #{train_plan.round_trip_price}円"
 
 # ホテル料金を算出するクラス
 class HotelPlan
-    SMOKER_COST = 1000
-    HOTEL_ROOM_RANK_ADD_FEES = {
-        "normal" => 0,
-        "bronze" => 3000,
-        "silver" =>  5000,
-        "gold" => 8000,
-    }
-    MEAL_ADD_FEES = {
-        "breakfast" => 500,
-        "dinner" => 800,
-    }
+  SMOKER_COST = 1000
+  HOTEL_ROOM_RANK_ADD_FEES = {
+    "normal" => 0,
+    "bronze" => 3000,
+    "silver" =>  5000,
+    "gold" => 8000,
+  }
+  MEAL_ADD_FEES = {
+    "breakfast" => 500,
+    "dinner" => 800,
+  }
 
-    def initialize(day_default_price)
-        @day_default_price = day_default_price
-    end
+  def initialize(day_default_price)
+    @day_default_price = day_default_price
+  end
 
-    # 喫煙可能部屋を利用時のコストを取得するメソッド
-    def is_smoker
-        @smoker_cost = SMOKER_COST
-    end
+  # 喫煙可能部屋を利用時のコストを取得するメソッド
+  def is_smoker
+    @smoker_cost = SMOKER_COST
+  end
 
-    # ホテルランクのコストを取得するメソッド
-    def select_hotel_rank(hotel_room_rank)
-        if HOTEL_ROOM_RANK_ADD_FEES.fetch(hotel_room_rank, nil).nil?
-            p "正しいランクを選択してください"
-            exit(0) 
-        else
-            @room_rank_cost = HOTEL_ROOM_RANK_ADD_FEES[hotel_room_rank]
-        end
+  # ホテルランクのコストを取得するメソッド
+  def select_hotel_rank(hotel_room_rank)
+    if HOTEL_ROOM_RANK_ADD_FEES.fetch(hotel_room_rank, nil).nil?
+      p "正しいランクを選択してください"
+      exit(0) 
+    else
+      @room_rank_cost = HOTEL_ROOM_RANK_ADD_FEES[hotel_room_rank]
     end
+  end
 
-    # 朝食利用時のコストを取得するメソッド
-    def has_breakfast
-        @breakfast_cost = MEAL_ADD_FEES["breakfast"]
-    end
- 
-    # 夕食利用時のコストを取得するメソッド
-    def has_dinner
-        @dinaner_cost = MEAL_ADD_FEES["dinner"]
-    end
+  # 朝食利用時のコストを取得するメソッド
+  def has_breakfast
+    @breakfast_cost = MEAL_ADD_FEES["breakfast"]
+  end
 
-    # ホテル料金合計を算出するメソッド
-    def hotel_price
-        @hotel_price = (@day_default_price + @smoker_cost + @room_rank_cost + @breakfast_cost + @dinaner_cost)
-    end
+  # 夕食利用時のコストを取得するメソッド
+  def has_dinner
+    @dinaner_cost = MEAL_ADD_FEES["dinner"]
+  end
+
+  # ホテル料金合計を算出するメソッド
+  def hotel_price
+    @hotel_price = (@day_default_price + @smoker_cost + @room_rank_cost + @breakfast_cost + @dinaner_cost)
+  end
 end
 
 hotel_plan = HotelPlan.new(5000)
@@ -79,16 +79,16 @@ puts "ホテル代: #{hotel_plan.hotel_price}円"
 
 # 旅費合計を算出するクラス
 class TravelPrice
-    def initialize(hotel_price, round_trip_price)
-        @hotel_price = hotel_price
-        @round_trip_price = round_trip_price
-    end
+  def initialize(hotel_price, round_trip_price)
+    @hotel_price = hotel_price
+    @round_trip_price = round_trip_price
+  end
 
-    # 旅費合計を算出するメソッド
-    def total_price
-        @total_price = @hotel_price + @round_trip_price
-        "旅費合計: #{@total_price}円"
-    end
+  # 旅費合計を算出するメソッド
+  def total_price
+    @total_price = @hotel_price + @round_trip_price
+    "旅費合計: #{@total_price}円"
+  end
 end
 
 travel_price = TravelPrice.new(hotel_plan.hotel_price, train_plan.round_trip_price)
