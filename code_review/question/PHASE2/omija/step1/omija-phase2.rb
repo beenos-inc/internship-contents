52088180d506d889157b20443354e3b31e337884
omija <oomija@tenso.com>
2018-07-05 16:29:51 +0900

    インスタン変数をメソッド内で作成していたのを修正

--- code_review/question/PHASE2/omija/step1/omija-phase2.rb
+++ code_review/question/PHASE2/omija/step1/omija-phase2.rb
@@ -1,25 +1,21 @@
 # 電車料金を算出するクラス
 class TrainPlan
-  STUDENT_DISCOUNT_COEFFICIENT = 0.2
-
-  def initialize(single_trip_price)
+  def initialize(single_trip_price, student_discount_rate)
     @single_trip_price = single_trip_price
+    @student_discount_rate = student_discount_rate
   end
 
   # 学割料金を算出するメソッド
-  def use_student_discount
-    @stuent_discount_cost = (@single_trip_price * STUDENT_DISCOUNT_COEFFICIENT).to_i
+  def use_student_discount(student_discount_rate)
+    (@single_trip_price * @student_discount_rate).to_i
   end
 
   # 往復代を算出するメソッド
-  def round_trip_price 
-    @round_trip_price = (@single_trip_price - @stuent_discount_cost) * 2
+  def round_trip_price
+    (@single_trip_price - use_student_discount(@student_discount_rate)) * 2
   end
 end
 
-train_plan = TrainPlan.new(12500)
-train_plan.use_student_discount
-puts "交通費: #{train_plan.round_trip_price}円"    
 
 # ホテル料金を算出するクラス
 class HotelPlan
@@ -35,61 +31,66 @@ class HotelPlan
     "dinner" => 800,
   }
 
-  def initialize(day_default_price)
+  def initialize(day_default_price, hotel_room_rank, is_smoker, has_breakfast, has_dinner)
     @day_default_price = day_default_price
-  end
-
-  # 喫煙可能部屋を利用時のコストを取得するメソッド
-  def is_smoker
-    @smoker_cost = SMOKER_COST
+    @hotel_room_rank = hotel_room_rank
+    @is_smoker = is_smoker
+    @has_breakfast = has_breakfast
+    @has_dinner = has_dinner
   end
 
   # ホテルランクのコストを取得するメソッド
-  def select_hotel_rank(hotel_room_rank)
-    if HOTEL_ROOM_RANK_ADD_FEES.fetch(hotel_room_rank, nil).nil?
+  def select_hotel_rank
+    if HOTEL_ROOM_RANK_ADD_FEES.fetch(@hotel_room_rank, nil).nil?
       p "正しいランクを選択してください"
       exit(0) 
     else
-      @room_rank_cost = HOTEL_ROOM_RANK_ADD_FEES[hotel_room_rank]
+      HOTEL_ROOM_RANK_ADD_FEES[@hotel_room_rank]
     end
   end
 
+  # 喫煙可能部屋を利用時のコストを取得するメソッド
+  def smoker_cost
+    SMOKER_COST
+  end
+
   # 朝食利用時のコストを取得するメソッド
-  def has_breakfast
-    @breakfast_cost = MEAL_ADD_FEES["breakfast"]
+  def breakfast_cost
+    MEAL_ADD_FEES["breakfast"]
   end
 
   # 夕食利用時のコストを取得するメソッド
-  def has_dinner
-    @dinaner_cost = MEAL_ADD_FEES["dinner"]
+  def dinner_cost
+    MEAL_ADD_FEES["dinner"]
   end
 
   # ホテル料金合計を算出するメソッド
   def hotel_price
-    @hotel_price = (@day_default_price + @smoker_cost + @room_rank_cost + @breakfast_cost + @dinaner_cost)
+    option_cost = 0
+    option_cost += select_hotel_rank
+    option_cost += smoker_cost if @is_smoker
+    option_cost += breakfast_cost if @has_breakfast
+    option_cost += dinner_cost if @has_dinner
+    @day_default_price + option_cost
   end
 end
 
-hotel_plan = HotelPlan.new(5000)
-hotel_plan.is_smoker
-hotel_plan.select_hotel_rank("gold")
-hotel_plan.has_breakfast
-hotel_plan.has_dinner
-puts "ホテル代: #{hotel_plan.hotel_price}円"
-
 # 旅費合計を算出するクラス
 class TravelPrice
-  def initialize(hotel_price, round_trip_price)
-    @hotel_price = hotel_price
+  def initialize(round_trip_price, hotel_price)
     @round_trip_price = round_trip_price
+    @hotel_price = hotel_price
   end
 
   # 旅費合計を算出するメソッド
   def total_price
-    @total_price = @hotel_price + @round_trip_price
-    "旅費合計: #{@total_price}円"
+    puts "交通費: #{@round_trip_price}円"
+    puts "ホテル代: #{@hotel_price}円"
+    puts "旅費合計: #{@round_trip_price + @hotel_price}円"
   end
 end
 
-travel_price = TravelPrice.new(hotel_plan.hotel_price, train_plan.round_trip_price)
-puts travel_price.total_price
+train_plan = TrainPlan.new(12500, 0.2)
+hotel_plan = HotelPlan.new(5000, "gold", true, true, true)
+travel_price = TravelPrice.new(train_plan.round_trip_price, hotel_plan.hotel_price)
+travel_price.total_price
