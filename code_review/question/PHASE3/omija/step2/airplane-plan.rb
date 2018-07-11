13923aa91be8bdd270142e905626b5a498c6eb9d
omija <oomija@tenso.com>
2018-07-11 11:31:16 +0900

    学割料金の引数を小数点から整数に変更、それに伴い学割料金の算出方法を変更 学割料金の引数が0~100の整数かどうか確認するメソッドを追加

---
+++ code_review/question/PHASE3/omija/step2/airplane-plan.rb
@@ -0,0 +1,34 @@
+# 飛行機料金を算出するクラス
+class AirplanePlan
+  def initialize(single_trip_price, student_discount_rate, use_early_reservation)
+    @single_trip_price = single_trip_price
+    @student_discount_rate = student_discount_rate
+    @use_early_reservation = use_early_reservation
+  end
+
+  # 学割率が正しい値かどうか確認し、正しい値であれば学割率渡すメソッド
+  def validate_student_discount_rate
+    if @student_discount_rate <= 100 && @student_discount_rate.is_a?(Integer)
+      @student_discount_rate
+    else
+      puts "学割の値は整数で0より大きく100未満の値を入れてください"
+      exit(0)
+    end
+  end
+
+  # 学割料金を算出するメソッド
+  def student_discount_fee
+    (@single_trip_price * validate_student_discount_rate) / 100
+  end
+
+  # 早期予約割引料金を算出するメソッド
+  def early_reservation_discount_fee
+    return 0 unless @use_early_reservation
+    (@single_trip_price * 0.3).to_i
+  end
+
+  # 往復代を算出するメソッド
+  def round_trip_price
+    (@single_trip_price - student_discount_fee - early_reservation_discount_fee) * 2
+  end
+end
---
+++ code_review/question/PHASE3/omija/step2/bus-plan.rb
@@ -0,0 +1,34 @@
+# バス料金を算出するクラス
+class BusPlan
+  def initialize(single_trip_price, student_discount_rate, use_coupon_discount)
+    @single_trip_price = single_trip_price
+    @student_discount_rate = student_discount_rate
+    @use_coupon_discount = use_coupon_discount
+  end
+
+  # 学割率が正しい値かどうか確認し、正しい値であれば学割率渡すメソッド
+  def validate_student_discount_rate
+    if @student_discount_rate <= 100 && @student_discount_rate.is_a?(Integer)
+      @student_discount_rate
+    else
+      puts "学割の値は整数で0より大きく100未満の値を入れてください"
+      exit(0)
+    end
+  end
+
+  # 学割料金を算出するメソッド
+  def student_discount_fee
+    (@single_trip_price * validate_student_discount_rate) / 100
+  end
+
+  # クーポン割引料金を算出するメソッド
+  def coupon_discount_fee
+    return 0 unless @use_coupon_discount
+    (@single_trip_price * 0.1).to_i
+  end
+
+  # 往復代を算出するメソッド
+  def round_trip_price
+    (@single_trip_price - coupon_discount_fee - student_discount_fee) * 2
+  end
+end
