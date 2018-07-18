efdd01eec2a3607f87371ea9a8f1feab374cfaf0
omija <oomija@tenso.com>
2018-07-18 18:14:20 +0900

    main.rb内の記述の並びを変更、不要なコメントアウトを削除

--- code_review/question/PHASE3/omija/step3/main.rb
+++ code_review/question/PHASE3/omija/step3/main.rb
@@ -4,11 +4,11 @@ require "./transport/airplane-plan.rb"
 require "./transport/bus-plan.rb"
 require "./travel-price.rb"
 
-# 飛行機代算出インスタンス作成
-airplane_plan = AirplanePlan.new(12_500, 20, true)
-
 # 電車代算出インスタンス作成
 train_plan = TrainPlan.new(12_500, 20, true)
+
+# 飛行機代算出インスタンス作成
+airplane_plan = AirplanePlan.new(12_500, 20, true)
 
 # バス代算出インスタンス作成
 bus_plan = BusPlan.new(12_500, 20, true)
@@ -16,8 +16,8 @@ bus_plan = BusPlan.new(12_500, 20, true)
 # ホテル代算出インスタンス作成
 hotel_plan = HotelPlan.new(5_000, :gold, true, true, true)
 
-# # 旅費算出インスタンス作成
+# 旅費算出インスタンス作成
 travel_price = TravelPrice.new(train_plan.round_trip_price, airplane_plan.round_trip_price, bus_plan.round_trip_price, hotel_plan.hotel_price)
 
-# # 旅費金額出力
+# 旅費金額出力
 travel_price.total_price
